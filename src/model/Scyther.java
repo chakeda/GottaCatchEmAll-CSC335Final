1650b32824e9b8405b20ea322a135cd9edc081a7
Steven_Murrell <stevenmurrell@email.arizona.edu>
2015-11-09 11:45:01 -0700

    Updated Pokemon class with throwPokeball and willRunAway methods, each returning boolean

--- src/model/Scyther.java
+++ src/model/Scyther.java
@@ -7,40 +7,38 @@ import javax.imageio.ImageIO;
 
 public class Scyther extends Pokemon{
 	
-	private static int RARITY = 7; // 1-10
-	private static int DURATION = 5; //5 minutes, after five minutes, pokemon will flee
-	private static int ESCAPABILITY = 75; // out of a possible 100, with 100 being it will always escape
-	private static int CATCHABILITY = 25; //out of a possible 100, with 100 being it will always be caught
+	private static final int RARITY = 7; // 1-10
+	private static final int DURATION = 5; //5 minutes, after five minutes, pokemon will flee
+	private static final int ESCAPABILITY = 50; // out of a possible 100, with 100 being it will always escape
+	private static final int CATCHABILITY = 75; //out of a possible 100, with 100 being it will always be caught
+	private static final int ESCAPABILITYCONSTANT = 10;
+	private static final int CATCHABILITYCONSTANT = 5;
+	
 	
 	public Scyther(){
 		this.setCatchability(CATCHABILITY);
 		this.setDuration(DURATION);
 		this.setEscapability(ESCAPABILITY);
 		this.setRarity(RARITY);
-		try{
+		try{ //for now, caughtImage is same as battle image (64x64)
 			this.setBattleImage(ImageIO.read(new File("./images/pokemonImages/scyther.gif")));
-		}catch (IOException e){
-			e.printStackTrace();
-		}
-		try{
 			this.setCaughtImage(ImageIO.read(new File("./images/pokemonImages/scyther.gif")));
 		}catch (IOException e){
 			e.printStackTrace();
 		}	
 	}
 	
-	//The +-10 or +-5 below are unique to each pokemon, can be changed later
-	//
+	
 	@Override
 	public void increaseEscapability() {
-		int newEscapability = this.getEscapability() + 10;
+		int newEscapability = this.getEscapability() + ESCAPABILITYCONSTANT;
 		if(newEscapability > 100)
 			newEscapability = 100;
 		this.setEscapability(newEscapability);
 	}
 	@Override
 	public void increaseCatchability() {
-		int newCatchAbility = this.getCatchability() + 5;
+		int newCatchAbility = this.getCatchability() + CATCHABILITYCONSTANT;
 		if(newCatchAbility > 100)
 			newCatchAbility = 100;
 		this.setCatchability(newCatchAbility);		
@@ -48,14 +46,14 @@ public class Scyther extends Pokemon{
 	
 	@Override
 	public void decreaseEscapability() {
-		int newEscapability = this.getEscapability() - 10;
+		int newEscapability = this.getEscapability() - ESCAPABILITYCONSTANT;
 		if(newEscapability < 0)
 			newEscapability = 0;
 		this.setEscapability(newEscapability);
 	}
 	@Override
 	public void decreaseCatchability() {
-		int newCatchAbility = this.getCatchability() - 5;
+		int newCatchAbility = this.getCatchability() - CATCHABILITYCONSTANT;
 		if(newCatchAbility < 0)
 			newCatchAbility = 0;
 		this.setCatchability(newCatchAbility);	
